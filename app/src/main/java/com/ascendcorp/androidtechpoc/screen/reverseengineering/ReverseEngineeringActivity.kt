aada92d5e9a09b425929f21b3182117772e737f2
yeneihub <44750404+yeneihub@users.noreply.github.com>
2022-09-27 14:02:08 +0700

    Add a new Reverse Engineering topic and update README (#4)
    * Add a reverse engineering topic
    
    * Add signing config for release and enable minify
    
    * Keep the super_sus_token to be in SusClass
    
    * Add a bonus step
    
    * Add a ProGuard rule to remove logs
    
    * Add a new line above Bonus section in Reverse Engineering topic
    
    * Remove unused code
    
    * Revert "Add a ProGuard rule to remove logs"
    
    This reverts commit 646027bbe0f7f27ade462d84d2ecd2ec67f25cb6.
    
    * Update README for reverse engineering topic
    
    * Try adding a new line before the image
    
    * Add newlines at end of files
    
    * Remove unused + sign
    
    Co-authored-by: Canapat Yostrakul <canapat.yos@ascendcorp.com>

---
+++ app/src/main/java/com/ascendcorp/androidtechpoc/screen/reverseengineering/ReverseEngineeringActivity.kt
@@ -0,0 +1,23 @@
+package com.ascendcorp.androidtechpoc.screen.reverseengineering
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.ascendcorp.androidtechpoc.databinding.ActivityReverseEngineeringBinding
+import com.ascendcorp.androidtechpoc.screen.navgraph.base.BaseActivity
+import com.ascendcorp.androidtechpoc.screen.reverseengineering.internal.SusClass
+
+class ReverseEngineeringActivity : BaseActivity<ActivityReverseEngineeringBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ActivityReverseEngineeringBinding
+        get() = {
+            ActivityReverseEngineeringBinding.inflate(it)
+        }
+
+    private val susClass = SusClass()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(this::class.java.simpleName, susClass.getToken())
+    }
+}
---
+++ app/src/main/java/com/ascendcorp/androidtechpoc/screen/reverseengineering/internal/SusClass.kt
@@ -0,0 +1,5 @@
+package com.ascendcorp.androidtechpoc.screen.reverseengineering.internal
+
+class SusClass {
+    fun getToken(): String = "super_sus_token"
+}
